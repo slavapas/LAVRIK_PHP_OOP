df553cffc20ab382f95d59e59bce8ec642b1a84d
Slava Pascaru <slavapascaru@gmail.com>
2022-02-17 21:49:38 +0200

    2. Inheritence (Extends) from a class1

--- Lesson 1/3. inheritance/index.php
+++ Lesson 1/3. inheritance/index.php
@@ -38,19 +38,80 @@ class Animal
 }
 
 // создаем наследование CAT от родителя ANIMAL
-class Cat extends Animal
-{
-}
-
 class Dog extends Animal
 {
 }
 
 
+class Cat extends Animal
+{
+  private $lifes;
+
+  public function __construct(string $name, int $health, int $power)
+  {
+    parent::__construct($name, $health, $power);
+    $this->lifes = 9;
+  }
+
+  private function hiddenLevel()
+  {
+    return $this->hiddenLevel;
+  }
+}
+
+
+class Mouse extends Animal
+{
+  private $hiddenLevel;
+
+  public function __construct(string $name, int $health, int $power)
+  {
+    parent::__construct($name, $health, $power);
+    $this->hiddenLevel = 0.4;
+  }
+
+  private function hiddenLevel()
+  {
+    return $this->hiddenLevel;
+  }
+}
+
+class GameCore
+{
+  private $units;
+
+  public function __construct()
+  {
+    $this->units = [];
+  }
+
+  public function addUnit($unit)
+  {
+    $this->unit[] = $unit;
+  }
+
+  public function nextTick()
+  {
+    foreach ($this->units as $unit) {
+      $damage = $unit->calcDamage();
+      print_r($damage);
+    }
+  }
+}
+
+
+$core = new GameCore();
 // создадим экземпляр или обьект кота
 $animal = new Animal("Гоша", 100, 10);
 $kisya = new Cat("Murka", 100, 10);
 $doggy = new Dog("Sharik", 100, 10);
+$micy = new Dog("Micy", 100, 10);
+
+$core->addUnit($kisya);
+$core->nextTick();
 // debug($murzik);
+
 echo "Животное " . $animal->calcDamage() . "\n";
-echo "Kisya " . $kisya->calcDamage();
+echo "Kisya " . $kisya->calcDamage() . "\n";
+echo "Doggya " . $doggy->calcDamage() . "\n";
+echo "Mousiya " . $micy->calcDamage() . "\n";
